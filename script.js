e3dbc12ffe4187fb6137f1549d9913af92a19fc2
Rambabu00 <123948696+Rambabu00@users.noreply.github.com>
2023-04-11 19:11:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+var firstN=document.getElementById("fname").value;
+	var lastN=document.getElementById("lname").value;
+	alert(firstN+" "+lastN);
 
 }
